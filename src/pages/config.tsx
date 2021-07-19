2a0883ba76936de98bc4e2cd2ebb072df9803347
侯志凯 <hou17513213314@163.com>
2021-07-19 15:30:25 +0800

    初始化文件

---
+++ src/pages/config.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export const Config = () => {
+  return (
+    <div>
+      <h1>配置页面</h1>
+      <Link to="/">返回首页</Link>
+    </div>
+  );
+};
